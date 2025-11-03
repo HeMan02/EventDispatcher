6dc1770adb396a8aecef0b2ee468b23b7844f027
Wilson <musso.luca89@gmail.com>
2025-11-03 15:52:04 +0100

    commit

---
+++ EventDispatcher.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class EventDispatcher
+{
+    //DIC ACTION
+    private Dictionary<IEvents.TypesEvents, Action<object>> _actions = new Dictionary<IEvents.TypesEvents, Action<object>>();
+
+    public void SetAction<T>(T value, IEvents.TypesEvents events)
+    {
+        object objectValue = value as object;
+
+        if (_actions.Keys.Contains(events))
+            _actions[events].Invoke(objectValue);
+    }
+
+    public void UnregisterAction<T>(Action<T> a, IEvents.TypesEvents events)
+    {
+        Action<object> actionCast = new Action<object>((o) => a((T)o));
+
+        if (_actions.ContainsKey(events))
+            _actions[events] -= actionCast;
+    }
+
+    public void RegisterAction<T>(Action<T> a, IEvents.TypesEvents events)
+    {
+        Action<object> actionCast = new Action<object>((o) => a((T)o));  
+
+        if (_actions.ContainsKey(events))
+            _actions[events] += actionCast;
+        else
+            _actions.Add(events, actionCast);
+    }
+}
+
